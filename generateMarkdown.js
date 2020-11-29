d383b906cf76e6139ec242dafd392660651e44e7
carlschr <cjcarlson136@gmail.com>
2020-11-28 18:08:25 -0600

    Add logic to prompt user and generate README

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
+  return `# ${data.title} <br>
+  ![license badge](https://img.shields.io/static/v1.svg?label=License&message=${data.license}&color=blue)<br>
+  ## Description <br>
+  ${data.description} <br>
+  ## Table of Contents <br>
+  * [Installation](#inst)<br>
+  * [Usage](#use)<br>
+  * [Contributing](#contr)<br>
+  * [Testing](#test)<br>
+  * [Questions](#questions)<br>
+  * [License](#license)<br>
+  ## <a id='inst'></a>Installation <br>
+  ${data.installation} <br>
+  ## <a id='use'></a>Usage <br>
+  ${data.usage} <br>
+  ## <a id='contr'></a>Contributing <br>
+  ${data.contributing} <br>
+  ## <a id='test'></a>Testing <br>
+  ${data.testing} <br>
+  ## <a id='questions'></a>Questions <br>
+  If you have any questions about this project, feel free to contact me on GitHub (my username is ${data.github}) or by email at ${data.email}. <br>
+  ## <a id='license'></a>License <br>
+  This project is under the ${data.license} license.`;
+};
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-// array of questions for user
-const questions = [
+//Inquirer module
+const inquirer = require('inquirer');
+//Filesystem module
+const fs = require('fs');
+//Markdown function
+const mark = require('./generateMarkdown');
 
-];
+// array of questions for user
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+}, {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+}, {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions for your project?',
+}, {
+    type: 'input',
+    name: 'usage',
+    message: 'What are the guidelines for using your project?',
+}, {
+    type: 'input',
+    name: 'contributing',
+    message: 'What are the guidelines for contributing to your project?',
+}, {
+    type: 'input',
+    name: 'testing',
+    message: 'How should a user go about testing your project?',
+}, {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['AFL', 'Apache', 'Artistic', 'BSL', 'BSD', 'CC', 'WTFPL', 'ECL', 'EPL', 'EUPL', 'AGPL', 'GPL', 'LGPL', 'ISC', 'LPPL', 'MS PL', 'MIT', 'MPL', 'OSL', 'NCSA', 'Unlicense', 'ZLib'],
+}, {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+}, {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+}];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    fs.writeFile(fileName, data, () => console.log('Your README.md file has been generated!'));
+};
 
 // function to initialize program
 function init() {
-
-}
+    inquirer.prompt(questions)
+    .then(answers => {
+        let markdown = mark(answers);
+        writeToFile('README.md', markdown);
+    });
+};
 
 // function call to initialize program
 init();
